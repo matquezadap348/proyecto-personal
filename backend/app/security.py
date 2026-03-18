e505a9a42d31b7854c8ff9aadae0b5f31cfb6a6b
matiasq <matiasquezada348@gmail.com>
2026-03-18 12:05:19 -0300

    dockerizar la API y conectar a PostgreSQL

--- backend/app/security.py
+++ backend/app/security.py
@@ -1,11 +1,14 @@
+import os
 import bcrypt
 import jwt
 from datetime import datetime, timedelta, timezone
+from dotenv import load_dotenv
 
+load_dotenv()
 
-SECRET_KEY = "clave_secreta_super_segura_para_desarrollo"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
 
 def hash_password(password: str) -> str:
     password_bytes = password.encode("utf-8")
@@ -13,19 +16,13 @@ def hash_password(password: str) -> str:
     return hashed_bytes.decode("utf-8")
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    
     plain_password_bytes = plain_password.encode("utf-8")
     hashed_password_bytes = hashed_password.encode("utf-8")
     return bcrypt.checkpw(plain_password_bytes, hashed_password_bytes)
 
 def create_access_token(data: dict) -> str:
     to_encode = data.copy()
-    
-    
     expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
-    
-  
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    
     return encoded_jwt
